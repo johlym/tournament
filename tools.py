f2790be65fe57189865fa86ab44015a43d27286b
Johnathan Lyman <jelyman@gmail.com>
2015-07-18 17:44:26 -0700

    fixed the logger.

--- tools.py
+++ tools.py
@@ -2,14 +2,14 @@ __author__ = 'jlyman'
 
 # extra but useful stuff
 
-import datetime, tournament as trn, uuid
+import datetime, database as db, uuid
 
 # the ability to log behaviors is always nice.
 
 def logger(entry, action):
     unique_id = str(uuid.uuid4())
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-    connection = trn.connect()
+    connection = db.connect()
     cursor = connection.cursor()
     cursor.execute("INSERT INTO auditlog "
                    "(entry, action, unique_id, timestamp) "
