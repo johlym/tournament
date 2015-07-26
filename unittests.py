13b50724316ff1abc2299eda6cb68f289d85d302
Johnathan Lyman <jelyman@gmail.com>
2015-07-25 18:20:24 -0700

    Created base unit tests structure.

---
+++ unittests.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# Test cases for tournament.py
+
+
+import database
+import match
+import unittest
+
+
+class TestDeleteMatch(unittest.TestCase):
+    def test(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
